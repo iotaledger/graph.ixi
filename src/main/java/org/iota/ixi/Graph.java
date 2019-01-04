fd4a2a4b4fbdb553326c966d38a15b159cc3ceec
Samuel <samuel.rufinatscha@gmail.com>
2019-01-04 14:22:51 +0100

    Implementing createVertex(String data, String[] edges): creates a vertex bundle fragment, returns the tail of it

--- src/main/java/org/iota/ixi/Graph.java
+++ src/main/java/org/iota/ixi/Graph.java
@@ -3,13 +3,15 @@ package org.iota.ixi;
 import org.iota.ict.ixi.DefaultIxiModule;
 import org.iota.ict.ixi.IctProxy;
 import org.iota.ict.model.Bundle;
+import org.iota.ict.model.BundleBuilder;
 import org.iota.ict.model.Transaction;
+import org.iota.ict.model.TransactionBuilder;
 import org.iota.ict.network.event.GossipReceiveEvent;
 import org.iota.ict.network.event.GossipSubmitEvent;
 import org.iota.ict.utils.Trytes;
-import org.iota.ixi.utils.VertexGenerator;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Graph extends DefaultIxiModule {
@@ -21,18 +23,16 @@ public class Graph extends DefaultIxiModule {
     @Override
     public void run() {
 
-        Bundle b = VertexGenerator.generateVertex();
-        for(Transaction t: b.getTransactions())
-            submit(t);
+        String hash = createVertex("DATAHASH9999999999999999999999999999999999999999999999999999999999999999999999999", new String[] { "FIRST9999999999999999999999999999999999999999999999999999999999999999999999999999", "SECOND999999999999999999999999999999999999999999999999999999999999999999999999999" } );
 
-        List<String> e = getEdges(b.getTransactions().get(0).hash);
+        List<String> e = getEdges(hash);
 
-        for(String s: e)
-            System.out.println(s);
+        System.out.println("VERTEX HASH: " + hash);
+        System.out.println("DATA: "+ getData(hash));
+        System.out.println("EDGE1: " + e.get(0));
+        System.out.println("EDGE2: " + e.get(1));
 
     }
-
-
 
     // returns all outgoing edges for the current vertex hash
     public List<String> getEdges(String vertex) {
@@ -64,6 +64,41 @@ public class Graph extends DefaultIxiModule {
         return t.extraDataDigest;
     }
 
+    // creates a vertex bundle fragment, returns the tail of it
+    public String createVertex(String data, String[] edges) {
+
+        BundleBuilder bundleBuilder = new BundleBuilder();
+        List<TransactionBuilder> transactions = new ArrayList<>();
+
+        TransactionBuilder t = new TransactionBuilder();
+        t.extraDataDigest = data;
+        t.signatureFragments = "";
+
+        for(String edge: edges) {
+
+            if(t.signatureFragments.length() < 27 * 81)
+                t.signatureFragments += edge;
+            else {
+                transactions.add(t);
+                t = new TransactionBuilder();
+            }
+
+        }
+
+        // fill signature fragment
+        t.signatureFragments = Trytes.padRight(t.signatureFragments, Transaction.Field.SIGNATURE_FRAGMENTS.tryteLength);
+        transactions.add(t);
+
+        Collections.reverse(transactions);
+        bundleBuilder.append(transactions);
+        Bundle vertexBundle = bundleBuilder.build();
+
+        for(Transaction tx: vertexBundle.getTransactions())
+            submit(tx);
+
+        return vertexBundle.getTransactions().get(0).hash;
+
+    }
 
     @Override
     public void onTransactionReceived(GossipReceiveEvent event) {
