5081239912499bdea30969bb348e01188451da0e
Samuel <samuel.rufinatscha@gmail.com>
2019-01-16 22:07:14 +0100

    Implementing reflected vertex structure, reworking methods, creating method documentation, updating to latest Ict.

--- src/main/java/org/iota/ixi/Graph.java
+++ src/main/java/org/iota/ixi/Graph.java
@@ -16,18 +16,17 @@ public class Graph extends IxiModule {
 
     public Graph(IctProxy ict) {
         super(ict);
-        new Thread(this).start();
     }
 
     @Override
-    public void run() {
+    public void run() { ; }
 
-        // List<TransactionBuilder> transactions = createVertex("DATAHASH9999999999999999999999999999999999999999999999999999999999999999999999999", new String[] { "FIRST9999999999999999999999999999999999999999999999999999999999999999999999999999", "SECOND999999999999999999999999999999999999999999999999999999999999999999999999999" } );
-
-    }
-
-
-    // returns the hash of the created head, trunk pointing to data, branch pointing to first edge
+    /**
+     * This method starts a reflected vertex with trunk pointing to the data and branch pointing to the first reflected vertex tail that is to be referenced.
+     * @param data the hash of the data bundle fragment tail
+     * @param edge the hash of the outgoing reflected vertex tail
+     * @return the hash of the reflected vertex head
+     */
     public String startVertex(String data, String edge) {
         TransactionBuilder transactionBuilder = new TransactionBuilder();
         transactionBuilder.trunkHash = data;
@@ -38,7 +37,12 @@ public class Graph extends IxiModule {
         return transaction.hash;
     }
 
-    // adds a transaction to the bundle started in startVertex, branch pointing to the edge, returns transaction hash
+    /**
+     * This method continues a reflected vertex with branch pointing to the next reflected vertex tail that is to be referenced.
+     * @param midVertexHash the hash of the reflected vertex tail to be continued
+     * @param edge the hash of the outgoing reflected vertex tail
+     * @return the hash of the new reflected vertex tail
+     */
     public String addEdge(String midVertexHash, String edge) {
         TransactionBuilder transactionBuilder = new TransactionBuilder();
         transactionBuilder.trunkHash = midVertexHash;
@@ -48,26 +52,29 @@ public class Graph extends IxiModule {
         return transaction.hash;
     }
 
-    // create the bundle fragment ready to attach to the tangle, or to put into a bundle, return the fragment tail
+    /**
+     * This method serializes a reflected vertex into a bundle fragment ready to put into a bundle.
+     * @param reflectedTail the hash of the reflected vertex tail to be finalized
+     * @return the bundle fragment ready to put into a bundle
+     */
     public List<TransactionBuilder> finalizeVertex(String reflectedTail) {
 
         List<String> edges = new LinkedList<>();
 
-        String current = reflectedTail;
         while(true) {
 
-            Transaction t = transactionsByHash.get(current);
+            Transaction t = transactionsByHash.get(reflectedTail);
             String edge = t.branchHash;
             edges.add(edge);
 
             if(Trytes.toTrits(t.tag)[0] == 1 || t.trunkHash.equals(Trytes.NULL_HASH)) // check if last transaction of vertex
                 break;
 
-            current = t.trunkHash;
+            reflectedTail = t.trunkHash;
 
         }
 
-        Transaction head = transactionsByHash.get(current);
+        Transaction head = transactionsByHash.get(reflectedTail);
         String data = head.trunkHash;
         String firstEdge = head.branchHash;
         edges.add(firstEdge);
@@ -89,51 +96,63 @@ public class Graph extends IxiModule {
 
         }
 
-        // fill signature fragment
+        // fill last signature fragment
         t.signatureFragments = Trytes.padRight(t.signatureFragments, Transaction.Field.SIGNATURE_FRAGMENTS.tryteLength);
+        t.tag = Trytes.padRight(Trytes.fromTrits(new byte[] { 1, 0, 0 }), Transaction.Field.TAG.tryteLength);
         transactions.add(t);
 
         return transactions;
     }
 
-    // returns all vertices for data hash
-    public List<String> getCompoundVertex(String dataHash) {
-        return verticesByDataHash.get(dataHash);
-    }
-
-    // returns all vertices with edges incoming to given vertex.
-    public List<String> getReferencingVertices(String vertex) {
-        return referencingVertices.get(vertex);
-    }
-
-    // returns all outgoing edges for the current vertex hash
-    public List<String> getEdges(String vertex) {
-
-        List<String> ret = new ArrayList<>();
-
-        while(true) {
-
-            Transaction t = transactionsByHash.get(vertex);
-
-            for(String edge: t.signatureFragments.split("(?<=\\G.{81})"))
-                if(!edge.equals(Trytes.NULL_HASH))
-                    ret.add(edge);
-
-            if(Trytes.toTrits(t.tag)[0] == 1 || t.trunkHash.equals(Trytes.NULL_HASH)) // check if last transaction of vertex
-                break;
-
-            vertex = t.trunkHash;
-
-        }
-
-        return ret;
-
-    }
-
-    // returns the hash of the data bundle fragment tail
+    /**
+     * This method returns the hash of the data bundle fragment tail.
+     * @param vertex the hash of the reflected vertex tail
+     * @return the hash of the data bundle fragment tail
+     */
     public String getData(String vertex) {
-        Transaction t = transactionsByHash.get(vertex);
-        return t.extraDataDigest;
+        Transaction transaction = null;
+        while(true) {
+            transaction = transactionsByHash.get(vertex);
+            if(Trytes.toTrits(transaction.tag)[0] == 1)
+                break;
+            if(transaction.trunkHash.equals(Trytes.NULL_HASH))
+                return null;
+            vertex = transaction.trunkHash;
+        }
+        return transaction.trunkHash;
+    }
+
+    /**
+     * This method returns all outgoing edges for the reflected vertex tail
+     * @param vertex the hash of the current reflected vertex tail
+     * @return all outgoing edges for the current reflected vertex tail
+     */
+    public List<String> getEdges(String vertex) {
+        List<String> ret = new ArrayList<>();
+        while(true) {
+            Transaction t = transactionsByHash.get(vertex);
+            if(t == null)
+                return ret;
+            ret.add(t.branchHash);
+            if(Trytes.toTrits(t.tag)[0] == 1 || t.trunkHash.equals(Trytes.NULL_HASH)) // check if last transaction of vertex
+                return ret;
+            vertex = t.trunkHash;
+        }
+    }
+
+    /**
+     * This method returns the next outgoing edge for the current reflected vertex tail
+     * @param vertex the hash of the current reflected vertex tail
+     * @param previousEdge the previous edge
+     * @return the next outgoing edge for the current reflected vertex tail
+     */
+    public String getNextEdge(String vertex, String previousEdge) {
+        List<String> edges = getEdges(vertex);
+        for(int i = 0; i < edges.size(); i++)
+            if(previousEdge.equals(edges.get(i)))
+                if(i + 1 < edges.size())
+                    return edges.get(i + 1);
+        return null;
     }
 
     // creates a vertex bundle fragment, returns the tail of it
@@ -182,13 +201,14 @@ public class Graph extends IxiModule {
          return null;
     }
 
-    public String getNextEdge(String vertex, String previousEdge) {
-        List<String> edges = getEdges(vertex);
-        for(int i = 0; i < edges.size(); i++)
-            if(previousEdge.equals(edges.get(i)))
-                if(i + 1 < edges.size())
-                    return edges.get(i + 1);
-        return null;
+    // returns all vertices for data hash
+    public List<String> getCompoundVertex(String dataHash) {
+        return verticesByDataHash.get(dataHash);
+    }
+
+    // returns all vertices with edges incoming to given vertex.
+    public List<String> getReferencingVertices(String vertex) {
+        return referencingVertices.get(vertex);
     }
 
 }
